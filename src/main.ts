1203c9ff5606ef4f95c286249fa90092cb4bf701
Katarzyna Kozlowska <Kat.w.kozlowska@gmail.com>
2023-11-24 15:47:00 +0000

    made changes to acss files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,29 @@
 import "./mian.css";
 
 const buttons = document.querySelectorAll<HTMLButtonElement>("button");
-if(!buttons) {throw new Error ("Eroor");
+const calculation = document.querySelector<HTMLParagraphElement>("p");
+const result = document.querySelector<HTMLHeadingElement>("h1");
+
+
+if(!buttons || !calculation || !result) {throw new Error ("Error");
 };
 
+
+//const handleButtonClick  = (event : Event) => {
+   //buttons.addEventListener("click", event => {
+      //  if (event.target.id.includes("num")) {
+       
+      //  }})
+   // };
+
+    
+
+// display of input from the user 
+
+
+// what happens when you click the button??
+
+
+//calculation 
+
+//special buttons % +/- etc. 
