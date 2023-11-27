e366688f57ecc7c9e0262e0778ebfdcb8b5455b5
Katarzyna Kozlowska <Kat.w.kozlowska@gmail.com>
2023-11-27 20:14:59 +0000

    the maths is better now !

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import "./mian.css";
 
 const buttons = document.querySelectorAll<HTMLButtonElement>("button");
-const result = document.querySelector<HTMLHeadingElement>("h1");
-const calculation = document.querySelector<HTMLParagraphElement>(".display__calculation");
+const resultDisplay = document.querySelector<HTMLHeadingElement>(".display__result");
+const num1Display = document.querySelector<HTMLParagraphElement>(".display__num1")
+const num2Display = document.querySelector<HTMLParagraphElement>(".display__num2")
+const operatorDisplay = document.querySelector<HTMLParagraphElement>(".display__operator")
 
 
-if(!buttons || !calculation || !result ) {throw new Error ("Error");
+if(!buttons || !resultDisplay || !num1Display || !num2Display || !operatorDisplay) {throw new Error ("Error");
 };
 
-let num1 = 0;
+let num1 = "";
 let operator = "";
-let num2 = 0;
+let num2 = "";
 let answer = 0;
 
 
@@ -18,71 +20,57 @@ let answer = 0;
 const handleButtonClick = (event: Event) => {
    // my event occurs on a html button > assign as button
    const button = event.currentTarget as HTMLButtonElement;
-   const buttonText = button.textContent as string;
+   const buttonText = button.textContent as string;  //as keyword means youre casting 
 
    if (buttonText == "C") {
-      result.innerText = "0";
-      calculation.innerText = "";
-      num1 = 0;
-      num2 = 0;
+      resultDisplay.innerText = "0";
+      num1 = "";
+      num2 = "";
       answer = 0;
       operator = "";
    } else if (button.classList.contains("number") ) {
-      if(num1 === 0){
-         num1 = parseInt(buttonText)
+      if(!operator){
+         num1 += buttonText;
          console.log("Num1: " + num1);
       } else if (num1 && operator) {
-         num2 = parseInt(buttonText)
+         num2 += buttonText
          console.log("Num2: " + num2);
       }
-      calculation.innerText += button.textContent as string;
-      result.innerText = button.textContent as string;
+
+      resultDisplay.innerText = button.textContent as string;
    } else if (button.classList.contains("operator")) {
          operator = buttonText
          console.log("Operator: " + operator);
    } else if (button.classList.contains("equals")) {
+      let value1:number = parseInt(num1);
+      let value2:number = parseInt(num2);
       switch (operator) {
          case "+":
-            answer= num1 + num2;
+            answer= value1 + value2;
             break;
          case "-":
-            answer =  num1 - num2;
+            answer =  value1 - value2;
             break;
          case "/":
-            answer = num1 / num2;
+            answer = value1 / value2;
             break;
          case "x":
-            answer = num1 * num2;
+            answer = value1 * value2;
             break;
       }
 
       console.log("Answer: " + answer);      
    }
 
-// console.log(num1);
-// console.log(operator);
-// console.log(num2);
-// console.log(answer);
- 
+   num1Display.innerText = "" + num1;
+   operatorDisplay.innerText = operator;
+   num2Display.innerText  = "" + num2;
+   resultDisplay.innerText = "" + answer;
+   
 };
 // for every button we found earlier
 
 buttons.forEach((button) => button.addEventListener("click", handleButtonClick));
 
-// number a, operator, number b 
 
-
-
-
-//delete function
-
-//computing 
-
-// i need the calculator to read the last number which i gave it 
-//listen for an operator
-// read the number i give it after the operator 
-//perform the maths 
-// update the display with the result 
-
-// (nice to have later ??? ) show history 
 
